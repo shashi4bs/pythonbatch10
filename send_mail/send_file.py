4111fd4c91c17e2d5ecf0d00101ae2a641bfacea
Your Name <you@example.com>
2017-09-29 08:11:09 +0530

    sending email using smtp server

--- send_mail/send_file.py
+++
@@ -1,7 +0,0 @@
-# import the smtplib module. It should be included in Python by default
-import smtplib
-from credz import MY_ADDRESS,PASSWORD
-# set up the SMTP server
-s = smtplib.SMTP(host='smtp.gmail.com', port=465)
-s.starttls()
-s.login(MY_ADDRESS, PASSWORD)
