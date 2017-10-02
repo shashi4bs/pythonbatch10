046bfcb81a1bdb6b8813a5705384cb514963d2fc
Your Name <you@example.com>
2017-10-02 14:22:39 +0530

    menu list

--- menu.py
+++ menu.py
@@ -1,13 +1,28 @@
+'''
+class menu
+'''
 class menu:
     name = []
     price = []
     def add(self,name,price=[]):
+        '''
+        ARGS:
+        name = item_name
+        price = item_price
+        '''
         self.name.append(name)
         self.price.append(price)
     def show(self):
         for i in range(len(self.name)):
             print(self.name[i],self.price[i])
+#instance of class menu
 c = menu()
+
+'''
+adds items and price to list
+c.add('item_name','item_price')
+
+'''
 c.add('idli',20)
 c.add('sambhar',10)
 c.show()                 
